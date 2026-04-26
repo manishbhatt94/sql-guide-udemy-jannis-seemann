57081e70ccb0e3eed27b897f582e06356d2153e1
Manish Bhatt <manishbhatt594@gmail.com>
2026-04-26 22:52:03 +0530

    Attempt to connect to Postgres

--- python-postgres/01-psycopg.py
+++ python-postgres/01-psycopg.py
@@ -1,3 +1,10 @@
 import psycopg
 
-print(psycopg)
+# Connect to an existing database
+with psycopg.connect(
+    "host=host.docker.internal port=5432 dbname=language_school user=rooter password=manish"
+) as conn:
+    print("Connection " + conn)
+    # Open a cursor to perform database operations
+    with conn.cursor() as cur:
+        ...
