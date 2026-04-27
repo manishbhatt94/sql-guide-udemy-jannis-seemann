f011c9ed6b615d5331135a4d00c1d34abbea2428
Manish Bhatt <manishbhatt594@gmail.com>
2026-04-27 13:38:51 +0530

    Move script inside new src directory

--- python-postgres-psycopg/src/01-psycopg.py
+++ python-postgres-psycopg/src/01-psycopg.py
@@ -2,9 +2,9 @@ import psycopg
 
 # Connect to an existing database
 with psycopg.connect(
-    "host=host.docker.internal port=5432 dbname=language_school user=rooter password=manish"
+    "host=localhost port=5432 dbname=language_school user=postgres password=manish"
 ) as conn:
-    print("Connection " + conn)
+    print(f"Connection: {conn}")
     # Open a cursor to perform database operations
     with conn.cursor() as cur:
         ...
